f2f860ec19b1bdb7df8fc34d402d26c07583ffb3
Manuel De Freitas <mdefreitas@bethel.jw.org>
2022-10-09 20:17:34 -0400

    Initial Commit

---
+++ Assemblee/AssembleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssembleeApp.swift
+//  Assemblee
+//
+//  Created by Manuel De Freitas on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AssembleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
